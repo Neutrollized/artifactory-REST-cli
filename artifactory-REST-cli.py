07c7a8c4cd1fac40855b5b0d5aa3320a2760fe1b
Neutrollized <glen.yu@gmail.com>
2018-08-27 22:04:39 -0400

    fixed some bugs, added functionality for --get and --add

--- artifactory-REST-cli.py
+++ artifactory-REST-cli.py
@@ -5,9 +5,10 @@
 #
 # example usage:
 # ./artifactory-REST-cli.py --add \
-#                           --repo mynpmrepo --repoclass npm \
-#                           --group mygroup --groupperm rd \
-#                           --perm myperm --public
+#                           --repo mynpmrepo --repopackage npm \
+#                           --group mygroup \
+#                           --perm myperm --groupperm rd \
+#                           --public
 #
 # will add a new (local) npm repository called 'mynpmrepo'
 # create a new (internal) group called 'mygroup' 
@@ -279,7 +280,7 @@ def addtoperm(perm_name, repo_name, group_name, group_perms, public_read):
     config = jq(".").transform(json.loads(p.text))
     config['repositories'] = new_repos
     # each user/group and perms are its own key:value pair
-    config['principals']['users']['anonymous'] = anonymous_perms
+    config['principals']['users'] = {'anonymous': anonymous_perms}
     config['principals']['groups'][group_name] = group_perms
 
     url = artifactory_url + 'security/permissions/' + perm_name
@@ -310,8 +311,6 @@ if __name__ == '__main__':
     group.add_argument('--group', type=str)
     group.add_argument('--grouprealm', default='internal', type=str,
                         help='internal | crowd | ldap')
-    group.add_argument('--groupperm', type=list,
-                        help='m=manage, d=delete, w=deploy, n=annotate, r=read')
     repo = parser.add_argument_group('repository')
     repo.add_argument('--repo', type=str)
     repo.add_argument('--repoclass', default='local', type=str,
@@ -320,6 +319,8 @@ if __name__ == '__main__':
                         help='generic | maven | npm , etc.')
     perm = parser.add_argument_group('permission')
     perm.add_argument('--perm', type=str)
+    perm.add_argument('--groupperm', type=list,
+                        help='m=manage, d=delete, w=deploy, n=annotate, r=read')
     perm.add_argument('--public', action='store_true', default=False,
                         help='enable public/anonymous user read')
     args = parser.parse_args()
@@ -332,12 +333,35 @@ if __name__ == '__main__':
     password = connection_info[2].strip('\n\r')
 
 
-    ### TEST ###
-    # action selected was --get
+    # action selected was --add
     if args.get:
-        g = getgroup(args.group)
-        print(g.text)
+        gr = getrepo(args.repo)
+        print(gr.text)
+        gg = getgroup(args.group)
+        print(gg.text)
+        gp = getperm(args.perm)
+        print(gp.text)
 
     # action selected was --add
     if args.add:
-        print('TODO')
+        print(args.repo, args.repoclass, args.repopackage)
+        print(args.group, args.grouprealm) 
+        print(args.perm, args.groupperm, args.public)
+
+        gr = getrepo(args.repo)
+        if gr.status_code == 400:    # create new repo 
+            cr = createrepo(args.repo, args.repoclass, args.repopackage)
+            print(cr.text)
+        
+        gg = getgroup(args.group)
+        if gg.status_code == 404:    # create new group
+            cg = creategroup(args.group, args.grouprealm)
+            print(cg.text)
+
+        gp = getperm(args.perm)
+        if gp.status_code == 404:    # create new perm entry
+            cp = createperm(args.perm, args.repo, args.group, args.groupperm, args.public)
+            print(cp.text)
+        elif gp.status_code == 200:    # perm already exists, modify existing
+            cp = addtoperm(args.perm, args.repo, args.group, args.groupperm, args.public)
+            print(cp.text)
